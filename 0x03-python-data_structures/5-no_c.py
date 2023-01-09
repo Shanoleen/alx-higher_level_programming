99ca28cc4fdf78c87ed37065e75816582c54a302
Shanoleen <shanoleen@live.com>
2023-01-09 15:26:56 -0800

    6

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,4 +5,4 @@ def no_c(my_string):
     for i in range(len(my_string)):
         if my_string[i] != 'c' and my_string[i] != 'C':
             new_string += my_string[i]
-            return new_string
+    return new_string
