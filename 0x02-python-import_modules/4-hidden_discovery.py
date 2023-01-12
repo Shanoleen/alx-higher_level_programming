56d1121185f7f8f443e4fb114f719e205e65997c
Shanoleen <shanoleen@live.com>
2023-01-12 04:35:29 -0800

    4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     """Print all names defined by hidden_4 module"""
     import hidden_4
+
     names = dir(hidden_4)
     for name in names:
         if name[:2] != "_":
