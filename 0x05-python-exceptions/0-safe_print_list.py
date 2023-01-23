701ec2bc08ef915a92dd417cec5d3be6fa95de7e
Shanoleen <shanoleen@live.com>
2023-01-23 04:34:27 -0800

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,5 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
         except IndexError:
             break
-        else:
-            counter += 1
-            print()
-            return counter
+        print("")
+        return counter
