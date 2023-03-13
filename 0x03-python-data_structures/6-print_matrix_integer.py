5cfaf146c7d9b9bb6490a1d350f98f2b5cdd98e2
Shanoleen <shanoleen@live.com>
2023-03-13 00:27:34 -0700

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     """Function that prints a matrix of integers"""
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-        if j != 0:
-            print(" ", end='')
-        print("{:d}".format(matrix[i][j]), end='')
-    print()
+            if j != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[i][j]), end='')
+        print()
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -28,7 +28,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 
-
 int is_palindrome(listint_t **head);
 
 
